6813a9f0663e040fb1d792632a054b041791a30a
Mohamed Esmael <109732874+mohamedesmael10@users.noreply.github.com>
2024-09-11 15:27:47 +0300

    Add files via upload

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 #terraform {
 #  backend "s3" {
 #    encrypt = true    
-#    bucket = "esmael-buckets"
+#    bucket = "esmael-bucket"
 #    dynamodb_table = "dynamodb-terraform-state-lock"
 #    key    = "terraform.tfstate"
 #    region = "us-east-1"
